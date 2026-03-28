bd1cc105d532553b6d749ea98d4362503335fcba
Nhat_Quang <23521278@gm.uit.edu.vn>
2026-03-29 02:14:27 +0700

    feat: add responsive sticky navbar and fix layout bugs

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/common/Navbar';
 import Home from './pages/player/Home';
 import FindPitch from './pages/player/FindPitch';
 import PitchDetail from './pages/player/PitchDetail';
@@ -8,14 +9,17 @@ import MatchBoard from './pages/player/MatchBoard';
 function App() {
   return (
     <Router>
-      <div className="min-h-screen bg-gray-50">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/tim-san" element={<FindPitch />} />
-          <Route path="/san/:id" element={<PitchDetail />} />
-          <Route path="/thanh-toan" element={<Checkout />} />
-          <Route path="/ghep-tran" element={<MatchBoard />} />
-        </Routes>
+      <div className="min-h-screen bg-gray-50 flex flex-col">
+        <Navbar />
+        <main className="flex-1 w-full mt-4">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/tim-san" element={<FindPitch />} />
+            <Route path="/san/:id" element={<PitchDetail />} />
+            <Route path="/thanh-toan" element={<Checkout />} />
+            <Route path="/ghep-tran" element={<MatchBoard />} />
+          </Routes>
+        </main>
       </div>
     </Router>
   );
